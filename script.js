ef8dd8d83e9d41635808a4aa529d8f9c6037bc6f
Momo Miller <minimomo14z@hotmail.com>
2022-09-02 16:50:09 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+// console.log("Yo!");
+// Create starter values
+// let monthlyIncome = 0;
+// let expenses = []; // An array of objects
+// let expenseTotal = 0;
+ 
+// Create references to HTML elements
+// let monthlyBudget = document.getElementById
+
+
+let monthlyBudget = document.getElementById
+
+function Budget () {
+    let monthlyIncome = `${$0}`
+    let expenses = [];
+    let expenseTotal = 0;
+    return;
+}
+
+Budget();
